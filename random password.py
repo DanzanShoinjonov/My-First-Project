db2e23d84cb28aca0fb99d60763b915963d03ade
Danzan <108413140+DanzanShoinjonov@users.noreply.github.com>
2022-10-02 16:05:45 +0800

    Update random password.py
    added discriptions

--- random password.py
+++ random password.py
@@ -1,17 +1,17 @@
 import random
-
+#добавление констант 
 digits = '1234567890'
 lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
 uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUCWXYZ'
 punctuation = '!#$%&*+-=?@^_'
 
-chars = ''
-
+chars = ''     #пустая переменная
+#запрос имени
 print('Добрый день, как вас зовут?')
 name = input()
 print('Добро пожаловать,', name,',в случайный генератор паролей.\nЗдесь вы можете создать случайный пароль, соответсвующий вашим критериям.')
 
-
+#характеристика пароля/ей (входные данные)
 print('Сколько паролей вам нужно?')
 kol_vo = int(input())
 print('Длина одного пароля должна быть ...')
@@ -27,7 +27,7 @@ simvols = input()
 print('Исключать ли неоднозначные символы "il1Lo0O"? (д - да, н - нет)')
 neodnazn_s = input()
 
-
+#входные данные, которые изменяют переменную "chars"
 if nums.lower() == 'д':
     chars += digits
 if propisn_b.lower() == 'д':
@@ -40,13 +40,13 @@ if neodnazn_s.lower() == 'д':
     for c in 'il1Lo0O':
         chars.replace(c, '')
 
-
+#основная функция
 def random_pass(long_s, chars):
     password = ''
-    for j in range(long_s):
+    for j in range(long_s): #цикл с длиной пароля, без него нижняя строка выдала бы только один символ
         password += random.choice(chars)
     print(password)
 
-
+#сколько паролей нужно пользователю, если "kol-vo", к примеру 2, до цикл сработает два раза, соответсвенно функция выдаст 2 пароля
 for _ in range(kol_vo):
     random_pass(long_s, chars)
